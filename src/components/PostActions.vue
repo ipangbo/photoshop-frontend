6b81ac9a80484f2ca0a6fb0485b07e9a5a0d8d6c
ipangbo <122402331@qq.com>
2023-01-10 23:03:17 +0800

    抽离PostActions组件

---
+++ src/components/PostActions.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="post-actions">
+    <TheIcon icon="like" fill="none" stroke="#000000"></TheIcon>
+    <span>1.5w</span>
+    <TheIcon icon="comment" fill="none" stroke="#000000"></TheIcon>
+    <span>1w</span>
+    <TheIcon icon="favorite" fill="none" stroke="#000000"></TheIcon>
+    <span>99w</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheIcon from "@/components/TheIcon.vue";
+</script>
+
+<style scoped>
+.post-actions {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  justify-items: center;
+  column-gap: 16px;
+  row-gap: 4px;
+}
+
+.post-actions > svg {
+  width: 32px;
+  height: 32px;
+  grid-row: 1 / 2;
+  cursor: pointer;
+}
+
+.post-actions > span {
+  font-size: 14px;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,14 +17,7 @@
             ></TheAvatar>
             <span>章鱼飞</span>
             <span class="post-pub-date">12小时之前发布</span>
-            <div class="post-actions">
-              <TheIcon icon="like" fill="none" stroke="#000000"></TheIcon>
-              <span>1.5w</span>
-              <TheIcon icon="comment" fill="none" stroke="#000000"></TheIcon>
-              <span>1w</span>
-              <TheIcon icon="favorite" fill="none" stroke="#000000"></TheIcon>
-              <span>99w</span>
-            </div>
+            <PostActions></PostActions>
           </div>
           <div class="post-desc">
             <p>
@@ -48,6 +41,7 @@
 <script setup lang="ts">
 import TheIcon from "@/components/TheIcon.vue";
 import TheAvatar from "@/components/TheAvatar.vue";
+import PostActions from "@/components/PostActions.vue";
 </script>
 
 <style scoped>
@@ -208,22 +202,5 @@ import TheAvatar from "@/components/TheAvatar.vue";
 }
 
 /*Post Actions*/
-.post-actions {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  justify-items: center;
-  column-gap: 16px;
-  row-gap: 4px;
-}
 
-.post-actions > svg {
-  width: 32px;
-  height: 32px;
-  grid-row: 1 / 2;
-  cursor: pointer;
-}
-
-.post-actions > span {
-  font-size: 14px;
-}
 </style>
